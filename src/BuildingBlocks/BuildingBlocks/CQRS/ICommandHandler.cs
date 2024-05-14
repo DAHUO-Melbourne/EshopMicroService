3eaadccdb2ec826c5e517716417d8c7ae7c1ec1b
DAHUO-Melbourne <huodamelbourne@gmail.com>
2024-05-15 00:02:03 +1000

    add CreateProductHandler

--- src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -9,6 +9,7 @@ namespace BuildingBlocks.CQRS
 {
     public interface ICommandHandler<in TCommand>: ICommandHandler<TCommand, Unit> where TCommand: ICommand<Unit>;
     public interface ICommandHandler<in TCommand, TResponse>: IRequestHandler<TCommand, TResponse> where TCommand : ICommand<TResponse> where TResponse : notnull
+        // in和out是用来定义<>泛型中的类型是用于输入的参数还是return value的类型的。in代表的是输入的参数类型，out是return的数据的类型
     {
     }
 }
