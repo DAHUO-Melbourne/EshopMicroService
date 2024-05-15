3f4444bbca3eea76d9dcf9ef412a3026f6e8a1a9
DAHUO-Melbourne <huodamelbourne@gmail.com>
2024-05-16 02:12:48 +1000

    feat: injection dependency

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
+// Add services to the container,dependency injection, 先把不同的服务依赖注入到前半部分
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
+// Configure the HTTP request pipeline，在后半部分：等到依赖注入完毕了，开始configuration不同的service
+
+app.MapCarter();
 
 app.Run();
