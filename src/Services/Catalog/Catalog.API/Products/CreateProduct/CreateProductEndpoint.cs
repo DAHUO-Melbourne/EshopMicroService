39655f75a38abda203eb3bc1f77b876fd9fdc770
DAHUO-Melbourne <huodamelbourne@gmail.com>
2024-05-16 01:18:17 +1000

    feat: add carter and mapster

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,16 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using Carter;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductEndpoint
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    // CreateProductRequest是request的class，collect all the info for creating a new product
+    public record CreateProductResponse(Guid Id);
+    // CreateProductResponse是response的class 
+    public class CreateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
