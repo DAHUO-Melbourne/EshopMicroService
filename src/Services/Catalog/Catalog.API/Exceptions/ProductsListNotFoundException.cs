f203df5677cd3fb0d99d7fce4ac1c47d6187d622
DAHUO-Melbourne <huodamelbourne@gmail.com>
2024-05-19 20:05:00 +1000

    fix: add getProductsByCategory

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductsListNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.API.Exceptions
+{
+    public class ProductsListNotFoundException: Exception
+    {
+        public ProductsListNotFoundException(): base("Product not Found!")
+        {
+            
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    internal class GetProductByCategoryQueryHandler
+        (IDocumentSession session, ILogger<GetProductByCategoryResult> logger)
+            : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByCategoryHandler.Handler called with {@query}", query);
+
+            var products = await session.Query<Product>().Where(p => p.Category.Contains(query.Category)).ToListAsync();
+
+            if (products == null)
+            {
+                throw new ProductsListNotFoundException();
+            }
+
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
