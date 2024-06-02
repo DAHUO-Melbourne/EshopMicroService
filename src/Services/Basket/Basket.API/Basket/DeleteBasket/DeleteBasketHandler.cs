148746b982afecf9752648d2959387489e606779
DAHUO-Melbourne <huodamelbourne@gmail.com>
2024-06-03 02:07:03 +1000

    feat: add delete basket handler

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,22 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿
+using Basket.API.Basket.StoreBasket;
+
+namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketHandler
+    public record DelectBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+    public record DeleteBasketResult(bool IsSuccess);
+    public class DeleteBasketCommandValidator : AbstractValidator<DelectBasketCommand>
     {
+        public DeleteBasketCommandValidator()
+        {
+            RuleFor(x => x.UserName).NotNull().WithMessage("UserName is required");
+        }
+    }
+    public class DeleteBasketCommandHandler : ICommandHandler<DelectBasketCommand, DeleteBasketResult>
+    {
+        public async Task<DeleteBasketResult> Handle(DelectBasketCommand command, CancellationToken cancellationToken)
+        {
+            return new DeleteBasketResult(true);
+        }
     }
 }
