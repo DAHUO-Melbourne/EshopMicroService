beb3e0a0e6373eba83869e30ce573ef302a97f1d
DAHUO-Melbourne <huodamelbourne@gmail.com>
2024-06-03 02:14:36 +1000

    feat: add delete basket endpoint

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -3,18 +3,18 @@ using Basket.API.Basket.StoreBasket;
 
 namespace Basket.API.Basket.DeleteBasket
 {
-    public record DelectBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+    public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
     public record DeleteBasketResult(bool IsSuccess);
-    public class DeleteBasketCommandValidator : AbstractValidator<DelectBasketCommand>
+    public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
     {
         public DeleteBasketCommandValidator()
         {
             RuleFor(x => x.UserName).NotNull().WithMessage("UserName is required");
         }
     }
-    public class DeleteBasketCommandHandler : ICommandHandler<DelectBasketCommand, DeleteBasketResult>
+    public class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
     {
-        public async Task<DeleteBasketResult> Handle(DelectBasketCommand command, CancellationToken cancellationToken)
+        public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
         {
             return new DeleteBasketResult(true);
         }
