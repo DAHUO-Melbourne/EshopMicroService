ab1ff1d73c833f062c15819b37403381bf2e3d17
DAHUO-Melbourne <huodamelbourne@gmail.com>
2024-06-03 02:32:59 +1000

    feat: register the services

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -2,8 +2,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // 在building以前进行依赖注入
 
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    // 给pipeline添加验证
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+    // 给pipeline添加log
+});
+
 var app = builder.Build();
 
 // 在building以后进行config
+app.MapCarter();
 
 app.Run();
